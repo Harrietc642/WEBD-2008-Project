b3c8d29cca1e2a18409949074aa1406dd4818af9
Harrietc642 <harriet.c642@gmail.com>
2021-11-05 14:02:41 -0500

    Created config.php and index.php

---
+++ config.php
@@ -0,0 +1,29 @@
+<?php
+/*
+Name: Harriet Chiu
+Date: September 28, 2021
+Course: Web Development 2
+Topic: 
+*/
+
+// date_default_timezone_set(UTC);
+
+define('DB_DSN', 'mysql:host=localhost;dbname=login;charset=utf8');
+define('DB_USER', 'project_admin');
+//define('DB_USER', 'serveruser');
+define('DB_PASS', 'gorgonzola7!');
+//
+$DSN='mysql:host = localhost; dbname=login';
+$ConnectingDB = new PDO($DSN,'root','');
+//
+
+try
+{
+	$db = new PDO(DB_DSN, DB_USER, DB_PASS);
+}
+catch (PDOException $e)
+{
+	print "Error: " . $e->getMessage();
+	die();
+}
+?>
