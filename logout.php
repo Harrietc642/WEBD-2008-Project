39329ba69734e949e6327e74d2049fbf81800c6a
Harrietc642 <harriet.c642@gmail.com>
2021-11-08 19:44:52 -0600

    Created logout.php

---
+++ logout.php
@@ -0,0 +1,20 @@
+<?php
+/*
+Name: Harriet Chiu
+Date: September 28, 2021
+Course: Web Development 2
+Topic: 
+*/
+
+// require_once("config.php");
+// session_reset();
+// session_destroy();
+// session_start();
+
+session_start();
+session_destroy();
+header("Location: index.php");
+
+
+
+?>
